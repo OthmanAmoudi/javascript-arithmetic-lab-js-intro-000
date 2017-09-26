8401fdd8144b13093b1f75f4bb74a46d41e1177c
Othman AMD <red.fantastic@hotmail.com>
2017-09-26 18:00:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,36 @@ function subtract(a,b){
   var result = a - b;
   return result;
 }
+
+function multiply(a,b){
+  var result = a * b;
+  return result;
+}
+
+function divide(a,b){
+  var result = a / b;
+  return result;
+}
+
+function inc(n){
+  var result = n++;
+  return result;
+}
+
+function dec(n){
+  var result = n--;
+  return result;
+}
+
+function makeInt(n){
+  var result = parseInt(n);
+  return result;
+}
+
+function preserveDecimal(n){
+  var result = parseFloat(n) ;
+  return result;
+}
+
+
+
